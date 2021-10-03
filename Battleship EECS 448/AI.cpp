7a288f424c5b6b3080f78d932fe656a47e5f4938
Musau Wambua <musau_w@ku.edu>
2021-10-02 19:45:34 -0500

    Skeleton of A.I. class

--- Battleship EECS 448/AI.cpp
+++ Battleship EECS 448/AI.cpp
@@ -1,1 +1,21 @@
+#include "AI.h"
+char selectOrientation(){
+  return('h');
+}
+std::tuple<int, int> place Ship(char orientation)
+  std::tuple<int, int> coordinates;
+  return(coordinates);
+}
+std::tuple<int, int> Shoot(){
+  std::tuple<int, int> coordinates;
+  return(coordinates);
+}
+void easyShoot(){
 
+}
+void mediumShoot(){
+
+}
+void hardShoot(){
+
+}
--- Battleship EECS 448/AI.h
+++ Battleship EECS 448/AI.h
@@ -1,1 +1,14 @@
-
+#ifndef AI_H
+#define AI_H
+class AI
+{
+  public:
+     char selectOrientation();
+     std::tuple<int, int> placeShip(char orientation);
+     std::tuple<int, int> Shoot();
+  private:
+    void easyShoot();
+    void mediumShoot();
+    void hardShoot();
+};
+#endif
